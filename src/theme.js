8903450ba1473b2fe421fb66c1c0274fadf58fbd
Le Hoang Long <lehoanglong9592@gmail.com>
2025-06-05 23:04:04 +0700

    setting mode dark, light or system

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,26 @@
 import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
+import { red, teal, deepOrange, cyan, orange } from '@mui/material/colors'
 
 // Create a theme instance.
 const theme = createTheme({
-  cssVariables: true,
+
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: teal,
+        secondary: deepOrange
+      }
+    },
+    dark: {
+      palette: {
+        primary: cyan,
+        secondary: orange
+      }
+    }
+  },
+  cssVariables: {
+    colorSchemeSelector: 'class'
+  },
   palette: {
     primary: {
       main: '#556cd6'
