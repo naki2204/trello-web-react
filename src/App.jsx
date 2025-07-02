ac70290827b6c1e1bd35d4f845b6556484506184
Le Hoang Long <lehoanglong9592@gmail.com>
2025-07-02 19:59:44 +0700

    refactor project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,9 @@
-import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-import HomeIcon from '@mui/icons-material/Home'
-import { pink } from '@mui/material/colors'
-import { useColorScheme } from '@mui/material/styles'
-
-import InputLabel from '@mui/material/InputLabel'
-import MenuItem from '@mui/material/MenuItem'
-import FormControl from '@mui/material/FormControl'
-import Select from '@mui/material/Select'
-
-import Box from '@mui/material/Box'
-
-import LightModeIcon from '@mui/icons-material/LightMode'
-import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
-import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
-
-function ModeSelect() {
-  const { mode, setMode } = useColorScheme()
-
-  const handleChange = (event) => {
-    const selectMode = event.target.value
-    setMode(selectMode)
-  }
-
-  return (
-    <div>
-      <FormControl sx={{ m: 1, minWidth: 80 }} size='small'>
-        <InputLabel id="label-select-dark-light">Mode</InputLabel>
-        <Select
-          labelId="label-select-dark-light"
-          id="demo-simple-select-autowidth"
-          value={mode}
-          onChange={handleChange}
-          autoWidth
-          label="Mode"
-        >
-          <MenuItem value="light">
-            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-              <LightModeIcon fontSize='small' /> Light
-            </Box>
-          </MenuItem>
-          <MenuItem value="dark">
-            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-              <DarkModeOutlinedIcon fontSize='small' /> Dark
-            </Box>
-          </MenuItem>
-          <MenuItem value="system">
-            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-              <SettingsBrightnessIcon fontSize='small' /> System
-            </Box>
-          </MenuItem>
-        </Select>
-      </FormControl>
-    </div>
-  )
-}
-
+import Board from './pages/Boards/_id'
 
 function App() {
   return (
     <>
-      <ModeSelect />
-      <hr/>
-      <div>hoanglongdev</div>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <br />
-
-      <AccessAlarmIcon />
-      <ThreeDRotation />
-      <HomeIcon />
-      <HomeIcon color="primary" />
-      <HomeIcon color="secondary" />
-      <HomeIcon color="success" />
-      <HomeIcon color="action" />
-      <HomeIcon color="disabled" />
-      <HomeIcon sx={{ color: pink[500] }} />
+      <Board />
     </>
   )
 }
---
+++ src/pages/Boards/_id.jsx
@@ -0,0 +1,16 @@
+import Container from '@mui/material/Container'
+import BoardBar from './BoardBar'
+import BoardContent from './BoardContent'
+import AppBar from '../../components/AppBar'
+
+function Board() {
+  return (
+    <Container disableGutters maxWidth={false} sx={{ height: '100vh', backgroundColor: 'primary.main' }}>
+      <AppBar />
+      <BoardBar />
+      <BoardContent />
+    </Container>
+  )
+}
+
+export default Board
